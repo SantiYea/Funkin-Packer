7c680dfd78acd652bf4cc98f4681f2d67699ff21
Homura <egzozu.be.bas@gmail.com>
2026-03-30 11:26:20 +0300

    sum languages

--- src/client/locale/languages.ts
+++ src/client/locale/languages.ts
@@ -1,44 +1,51 @@
 import * as en from "./languages/en.json";
+import * as ar from "./languages/ar.json";
+import * as zh from "./languages/zh-cn.json";
 import * as de from "./languages/de.json";
 import * as es from "./languages/es.json";
-import * as ru from "./languages/ru.json";
-import * as zh from "./languages/zh-cn.json";
-import * as pt_br from "./languages/pt-br.json";
 import * as ca_fr from "./languages/ca-fr.json";
 import * as it from "./languages/it.json";
+import * as pt_br from "./languages/pt-br.json";
+import * as tr from "./languages/tr.json";
+import * as uk from "./languages/uk.json";
+import * as ru from "./languages/ru.json";
 
 export type Language = {
-	lang: string;
-	name: string;
-	mapping: Record<string, string>;
-}
+  lang: string;
+  name: string;
+  mapping: Record<string, string>;
+  isRtl?: boolean;
+};
 
 export function getLanguageByCode(code: string): Language {
-	for (const item of languages) {
-		if (item.lang === code) return item;
-	}
-	return languages[0];
+  for (const item of languages) {
+    if (item.lang === code) return item;
+  }
+  return languages[0];
 }
 
 export const languages: Language[] = [
-	{ lang: "en", name: "english", mapping: en },
-	{ lang: "de", name: "deutsch", mapping: de },
-	{ lang: "es", name: "español", mapping: es },
-	{ lang: "ru", name: "русский", mapping: ru },
-	{ lang: "zh-cn", name: "简体中文", mapping: zh },
-	{ lang: "pt-br", name: "português (brasil)", mapping: pt_br }, // by @CrowPlexus
-	{ lang: "ca-fr", name: "français (canadien)", mapping: ca_fr }, // by @thatoneidiotxav
-	{ lang: "it", name: "italiano", mapping: it }, // by @NexIsDumb
+  { lang: "en", name: "english", mapping: en },
+  { lang: "ar", name: "العربية", mapping: ar, isRtl: false }, // by @KarimAkra
+  { lang: "zh-cn", name: "简体中文", mapping: zh },
+  { lang: "de", name: "deutsch", mapping: de },
+  { lang: "es", name: "español", mapping: es },
+  { lang: "ca-fr", name: "français (canadien)", mapping: ca_fr }, // by @thatoneidiotxav
+  { lang: "it", name: "italiano", mapping: it }, // by @NexIsDumb
+  { lang: "pt-br", name: "português (brasil)", mapping: pt_br }, // by @CrowPlexus
+  { lang: "tr", name: "türkçe", mapping: tr }, // by @HomuHomu833 and @ArkoseLabsOfficial
+  { lang: "uk", name: "українська", mapping: uk }, // by @mrchaoss1
+  { lang: "ru", name: "русский", mapping: ru }, // by @mrchaoss1
 ] as const;
 
 const ALL_KEYS = Object.keys(languages[0].mapping);
 
-if(DEBUG) {
-	for(const item of languages) {
-		for(const key of ALL_KEYS) {
-			if(!item.mapping[key]) {
-				console.warn(`Missing key in language ${item.lang}.json: ${key}`);
-			}
-		}
-	}
+if (DEBUG) {
+  for (const item of languages) {
+    for (const key of ALL_KEYS) {
+      if (!item.mapping[key]) {
+        console.warn(`Missing key in language ${item.lang}.json: ${key}`);
+      }
+    }
+  }
 }
